4e295c264de2ce2ef8a26469495b1731a41e09c3
Emmanuel <emmanuelokello294@gmail.com>
2024-03-16 18:52:12 +0300

    completes evente acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+function moveDodgerLeft() {
+    
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const currentLeft = parseInt(dodger.style.left, 10);
+  
+    
+    const maxRight = window.innerWidth - dodger.offsetWidth; 
+  
+    
+    if (currentLeft < maxRight) {
+      dodger.style.left = `${currentLeft + 1}px`; 
+    }
+  }
+  
+    
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }else if(e.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+
+  });
+
